663ac2b27561ba3a5ea45057be694dfc08c041a8
Patricia Barrios <patricia.barriospalacios@telefonica.com>
2018-04-30 10:51:41 +0200

    fix replace fragment bug

--- app/src/main/java/com/example/gangame/MainActivity.kt
+++ app/src/main/java/com/example/gangame/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.gangame
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import com.example.commons.BaseFragment
 import com.example.gangame.deals.DealsFragment
 import com.example.gangame.owned.TopOwnedFragment
 import com.example.gangame.rated.TopRatedFragment
@@ -11,10 +10,10 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     companion object {
-        const val DEAFULT_OPTION =  R.id.action_deals
+        const val DEFAULT_OPTION =  R.id.action_deals
     }
 
-    val fragments : HashMap<Int, BaseFragment> = hashMapOf(
+    private val fragments : HashMap<Int, Fragment> = hashMapOf(
             Pair(R.id.action_deals, DealsFragment()),
             Pair(R.id.action_top_rated, TopRatedFragment()),
             Pair(R.id.action_most_owned, TopOwnedFragment())
@@ -24,20 +23,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navigationView.selectedItemId = DEAFULT_OPTION
+        initView()
+
+        navigationView.selectedItemId = DEFAULT_OPTION
         navigationView.setOnNavigationItemSelectedListener { item ->
-            val fragment : BaseFragment? = fragments[item.itemId]
+            val fragment : Fragment? = fragments[item.itemId]
+
+            if(fragment != null){
+                replaceFragment(fragment)
+            }
+
             true
         }
     }
 
-    fun initView(){
-        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer)
+    private fun replaceFragment(fragment: Fragment?){
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragmentContainer, fragment)
+                .commit()
+    }
 
+    private fun initView(){
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer)
         if(currentFragment == null){
             supportFragmentManager
                     .beginTransaction()
-                    .add(R.id.fragmentContainer, fragments[DEAFULT_OPTION] as Fragment)
+                    .add(R.id.fragmentContainer, fragments[DEFAULT_OPTION])
                     .commit()
         }
     }
---
+++ commons/src/main/java/com/example/commons/BaseListFragment.kt
@@ -0,0 +1,5 @@
+package com.example.commons
+
+/**
+ * Created by cxb0163 on 30/04/2018.
+ */
